1dbf7b4a9e965704e88ca9329f044ed4f6ac6a8c
Tran-Kieu <tranh@brlogin2.ib>
2026-08-05 14:29:53 -0600

    Updated Python plot script for verification Example 2: increased font size

--- verification/Verification_TE/Comsol_comp/Example_2/plot_K.py
+++ verification/Verification_TE/Comsol_comp/Example_2/plot_K.py
@@ -26,22 +26,23 @@ data_comsol_T['T'] = [temp for temp in data_comsol_T['T']]  # COMSOL temperature
 
 # Plot setup
 plt.figure(figsize=(12, 8), dpi=1000)
-plt.rc('font', family='sans-serif', size=16)
+plt.rc('font', family='sans-serif', size=22)
 ax = plt.subplot(1, 1, 1)
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
-plt.xlabel("Time [s]")
-plt.ylabel("Cold side temperature [K]")  # Updated Y-axis label for Kelvin
+plt.xlabel("Time [s]", fontsize=22)
+plt.ylabel("Cold side temperature [K]", fontsize=22)  # Updated Y-axis label for Kelvin
 
 # Plot data
 plt.plot(data['time'], data['T_cold'], linestyle='-', marker='', color='cornflowerblue')  # MOOSE data
-plt.plot(data_comsol_T["time"], data_comsol_T["T"], linestyle='', marker='o', color='orange')  # COMSOL data
+plt.plot(data_comsol_T["time"], data_comsol_T["T"], linestyle='--', color='orange')  # COMSOL data
 
 # Add legend
-ax.legend(['MOOSE', "COMSOL"], frameon=False, prop={'size': 16}, loc='lower right')
+ax.legend(['MOOSE', "COMSOL"], frameon=False, prop={'size': 22}, loc='lower right')
 # plt.grid() # Tran-Kieu: Added 6/29/2026 
 
 # Finalize and save plot
-plt.xlim(0, 20) # Tran-Kieu: Added 6/29/2026 
+ax.tick_params(axis='both', labelsize=20)
+# plt.xlim(0, 20) # Tran-Kieu: Added 6/29/2026 
 plt.ylim(215, 222) # Tran-Kieu: Added 6/29/2026 
 plt.tight_layout()
 plt.savefig('T_cold_K.png')  # Updated filename to reflect Kelvin
